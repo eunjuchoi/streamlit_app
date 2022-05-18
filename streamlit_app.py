e234e62fcd3279cf31257a16be68a880dd577357
eunjuchoi <105779294+eunjuchoi@users.noreply.github.com>
2022-05-18 18:14:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 
 st.title("streamlit App Test")
 st.header("최은주")
@@ -10,3 +11,6 @@ col1, col2, col3 = st.columns(3)
 col1.metric("temperature","70","1.2")
 col2.metric("temperature","70","1.2")
 col3.metric("temperature","70","1.2")
+d=st.date_input(
+  "when's your birthday", datetime.date(2022,1,3))
+st.write("your birthday is:',d)
